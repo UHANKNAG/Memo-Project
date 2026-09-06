7f95cc92b6939ee245cee8189b1d4deab3975c78
UHANGNAG <hhs095615@gmail.com>
2026-09-07 03:05:31 +0900

    메모장 프로젝트 Memo Database 연결하기

--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -14,6 +14,13 @@ public class MemoResponseDto {
         this.username = memo.getUsername();
         this.contents = memo.getContents();
     }
+
+    public MemoResponseDto(Long id, String username, String contents) {
+        this.id = id;
+        this.username = username;
+        this.contents = contents;
+
+    }
 }
 
 // 따로 생성하는 이유: DB와 소통하는 class는 조심스럽게 다루어야 하기 때문에
